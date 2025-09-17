4578b45e5fd2242589f9755d8a7faa4816b72e57
sumeyye <sumeyyekirtil4@gmail.com>
2025-09-18 00:25:33 +0300

    set product class

--- Models/Product.cs
+++ Models/Product.cs
@@ -6,11 +6,8 @@ namespace MVCStockAreas.Models
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
-		public string? Photo { get; set; }
 		public decimal Price { get; set; }
 		public int Stock { get; set; }
-
-		[Display(Name = "Resim"), StringLength(100)]
 		public string? Image { get; set; }
 		public DateTime RecordDate { get; set; } = DateTime.Now;
 	}
