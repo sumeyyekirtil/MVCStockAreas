1431e9266906c4f9d2c4707eae08cb225f192c42
sumeyye <sumeyyekirtil4@gmail.com>
2025-09-17 20:00:47 +0300

    product property class

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,4 +1,6 @@
-﻿namespace MVCStockAreas.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCStockAreas.Models
 {//kullanılacak ürün sınıfı
 	public class Product
 	{
@@ -7,6 +9,9 @@
 		public string? Photo { get; set; }
 		public decimal Price { get; set; }
 		public int Stock { get; set; }
+
+		[Display(Name = "Resim"), StringLength(100)]
+		public string? Image { get; set; }
 		public DateTime RecordDate { get; set; } = DateTime.Now;
 	}
 }
