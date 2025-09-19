b4849361f9f6bdb1ce1b69c30dddb8bd46f6a326
sumeyye <sumeyyekirtil4@gmail.com>
2025-09-19 19:00:36 +0300

    model Product class

--- Models/Product.cs
+++ Models/Product.cs
@@ -5,10 +5,20 @@ namespace MVCStockAreas.Models
 	public class Product
 	{
 		public int Id { get; set; }
+
+		[Display(Name = "İsim")] //ekran görünüm adı ayarlandı
 		public string Name { get; set; }
+
+		[Display(Name = "Fiyat")]
 		public decimal Price { get; set; }
+
+		[Display(Name = "Stok")]
 		public int Stock { get; set; }
+
+		[Display(Name = "Resim")]
 		public string? Image { get; set; }
+
+		[Display(Name = "Kayıt Tarihi")]
 		public DateTime RecordDate { get; set; } = DateTime.Now;
 	}
 }
