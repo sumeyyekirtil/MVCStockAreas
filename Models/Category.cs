25a8b916f2be0da61e00f6fc82ece51713ec5b47
sumeyye <sumeyyekirtil4@gmail.com>
2025-09-19 18:59:04 +0300

    model category class

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,11 +1,21 @@
-﻿namespace MVCStockAreas.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCStockAreas.Models
 {//kullanılacak kategori sınıfı
 	public class Category
 	{
 		public int Id { get; set; }
+
+		[Display(Name = "İsim")] //ekranda görünüm ayarlandı
 		public string Name { get; set; }
+
+		[Display(Name = "Açıklama")]
 		public string? Description { get; set; }
+
+		[Display(Name = "Aktif")]
 		public bool Active { get; set; }
+
+		[Display(Name = "Oluşturulma Tarihi")]
 		public DateTime CreateDate { get; set; } = DateTime.Now;
 	}
 }
